307be06acd17b4f45ae318e95dcd826c80aba456
Johann Schuster <Johann.Schuster@motortrend.com>
2021-01-06 08:28:12 -0800

    mergeSort

---
+++ mergeSort.js
@@ -0,0 +1,27 @@
+function merge(arr1, arr2) {
+    let results = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < arr1[i] && j < arr2[j]) {
+        if (arr2[j] > arr1[i]) {
+            results.push(arr1[i]);
+            i++;
+        } else {
+            results.push(arr2[j]);
+            j++;
+        }
+    }
+
+    while (i < arr1.length) {
+        results.push(arr1[i]);
+        i++;
+    }
+
+    while (j < arr2.length) {
+        results.push(arr2[j]);
+        j++;
+    }
+
+    return results;
+}
