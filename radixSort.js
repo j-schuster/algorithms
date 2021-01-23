1e666fa42b65bebc985440c4d3b83b785cceccae
Johann Schuster <Johann.Schuster@motortrend.com>
2021-01-23 07:16:16 -0800

    linkedlistpopandpush

--- radixSort.js
+++ radixSort.js
@@ -4,7 +4,12 @@
     It orgninizes numbers by looking at the amount of numbers in the number
     Thus, any three number digit will be more than any 2 digit number
     We sort these into 10 buckets (base 10) according to the last number on the right
+
+    BigO = o(nk)
+
+    not ideal if numbers are very large 
  */
+
 
 function getDigit(num, i) {
     return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
@@ -22,3 +27,19 @@ function mostDigits(nums) {
     }
     return maxDigits;
 }
+
+function radixSort(nums) {
+  let maxDigitCount = mostDigits(nums);
+  
+  for(k = 0; k < maxDigitCount; k++){
+    const buckets = Array.from({ length: 10 }, () => []);
+    for(let i = 0; i < nums.length; i++){
+      const digit = getDigit(nums[i], k);
+      buckets[digit].push(nums[i]);
+    }
+    nums = [].concat(...buckets);
+  }
+  return nums;
+}
+
+console.log(radixSort([311,43462,54, 3, 19, 29475, 78, 4,2425245]));
---
+++ singlyLinkedList.js
@@ -0,0 +1,65 @@
+/*
+    Singly Linked Lists are data structures that contain a head, tail and length props
+    No indeces butt nodes that point to the next
+    ie there is no random access because no need to re-index everything
+
+    the data structure needs the following methods on it 
+
+    push
+    pop
+*/
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        const newNode = new Node(val);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        if (!this.head) return undefined;
+
+        let current = this.head;
+        let newTail = current;
+
+        while (current.next) {
+            newTail = current;
+            current = current.next;
+        }
+
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
+}
+
+const linkedList = new SinglyLinkedList();
+linkedList.push('hello');
+linkedList.push('there');
+linkedList.push('bro');
